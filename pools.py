777608e723d71b684291f38100446b071242064d
shridharivani7 <shridharsjce@gmail.com>
2016-12-27 16:18:42 +0530

    Update pools.py

--- pools.py
+++ pools.py
@@ -26,4 +26,40 @@ if __name__ == "__main__":
     pool.close()
     pool.join()
     
+    
+    
+#Revised Way to do
+from multiprocessing import Pool, TimeoutError
+import time
+import requests
+import json
+def fetch_url(i):
+    url="http://surya-interview.appspot.com/message"
+    
+    headers={'X-Surya-Email-Id':'shridharsjce@gmail.com'}
+    #request.get function
+    r=requests.get(url,headers=headers)
+    x=r.json()
+    #print "Request :", r.status_code,x
+    #request.post function
+    data=x
+    r=requests.post(url,data=json.dumps(x))
+    if i in [10,50,90,95,99,100,101]:
+        if i == 100:
+            print "Mean"
+        elif i == 101:
+            print "Standard Deviation"
+        else:
+            print i,"th Percentile"
+        return 
+    
+if __name__ == "__main__":
+    jobs = []
+    pool = Pool(processes=10)
+    pool.map(fetch_url, range(102))
+    
+    pool.close()
+    pool.join()
+
+    
 
